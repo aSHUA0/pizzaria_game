2484e74cd8ff5aa75a86899575f7760b1468cd3e
aSHUA0 <guilhermemengo6@gmail.com>
2025-05-26 16:36:21 -0300

    update game1

--- src/game1.py
+++ src/game1.py
@@ -1,5 +1,4 @@
 import pygame
-import math
 
 pygame.init()
 WIDTH, HEIGHT = 1250, 900
@@ -12,6 +11,8 @@ espaco_entre_clientes = 40
 raio = 30
 movendoMassa = False
 movendoRolo = False
+movendoMassaAberta = False
+mostrarMassa = True
 
 
 # Y fixo para todos os clientes (acima da mesa)
@@ -29,11 +30,11 @@ for i in range(num_clientes):
 
 mesa = pygame.Rect(0, HEIGHT - altura_mesa, WIDTH, altura_mesa)
 massa = pygame.Rect(100, 100, 100, 100)
+massa_aberta = pygame.Rect(150, 100, 100, 150)
 rolo = pygame.Rect(100, 500, 100, 50)
 
 # Cor da mesa
 cor_mesa = (100, 50, 20)
-
 
 running = True
 while running:
@@ -46,24 +47,37 @@ while running:
                 movendoMassa = not movendoMassa
             elif rolo.collidepoint(pygame.mouse.get_pos()):
                 movendoRolo = not movendoRolo
+            elif massa_aberta.collidepoint(pygame.mouse.get_pos()):
+                movendoMassaAberta = not movendoMassaAberta
 
     if movendoMassa:
         mouse_x, mouse_y = pygame.mouse.get_pos()
         massa.center = (mouse_x, mouse_y)
     
+    if movendoMassaAberta:
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        massa_aberta.center = (mouse_x, mouse_y)
+
     if movendoRolo:
         mouse_x, mouse_y = pygame.mouse.get_pos()
         rolo.center = (mouse_x, mouse_y)
-
+        if massa.center == rolo.center:
+            mostrarMassa = False
 
     screen.fill((255, 255, 255))  # fundo branco
+    
     pygame.draw.rect(screen, cor_mesa, mesa)
-
-    pygame.draw.rect(screen, (0, 255, 0), massa)
-    pygame.draw.rect(screen, (255, 255, 0), rolo)
-
+    
     for i in range(len(clientes)):
         pygame.draw.rect(screen, (0, 0, 0), clientes[i])
+
+    if mostrarMassa:
+        pygame.draw.rect(screen, (0, 255, 0), massa)
+    else:
+        pygame.draw.rect(screen, (0, 255, 0), massa_aberta)
+
+
+    pygame.draw.rect(screen, (255, 255, 0), rolo)
 
 
     pygame.display.flip()
