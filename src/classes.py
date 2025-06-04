d7400075948c35302d9d79b45eea613eb5903ae4
aSHUA0 <guilhermemengo6@gmail.com>
2025-06-03 23:55:37 -0300

    create a class file

---
+++ src/classes.py
@@ -0,0 +1,53 @@
+import pygame
+import threading
+import time
+
+class Cliente(threading.Thread):
+    def __init__(self, id_cliente, rect, massa_aberta, pizza_semaforo, clientes_estado):
+        super().__init__()
+        self.id = id_cliente
+        self.rect = rect
+        self.massa_aberta = massa_aberta
+        self.pizza_semaforo = pizza_semaforo
+        self.clientes_estado = clientes_estado
+        self.running = True
+
+    def run(self):
+        self.clientes_estado[self.id] = "esperando"
+
+        while not self.detecta_pizza():
+            time.sleep(0.1)
+            if not self.running:
+                return
+
+        self.pizza_semaforo.acquire()
+        self.clientes_estado[self.id] = "comendo"
+        time.sleep(2)
+        self.clientes_estado[self.id] = "idle"
+        self.pizza_semaforo.release()
+
+    def detecta_pizza(self):
+        return self.massa_aberta.colliderect(self.rect)
+    
+class ObjetoFisico:
+    def __init__(self, x, y, largura, altura, cor, gravidade=0.01):
+        self.rect = pygame.Rect(x, y, largura, altura)
+        self.vel_queda = 0
+        self.caindo = False
+        self.gravidade = gravidade
+        self.cor = cor
+
+    def aplicar_gravidade(self, chao):
+        if self.caindo:
+            self.vel_queda += self.gravidade
+            self.rect.y += self.vel_queda
+            if self.rect.bottom >= chao:
+                self.rect.bottom = chao
+                self.caindo = False
+                self.vel_queda = 0
+
+    def mover_para_mouse(self, mouse_pos, limite):
+        if not limite.collidepoint(mouse_pos):
+            self.caindo = True
+            self.vel_queda = 0
+        self.rect.center = mouse_pos
