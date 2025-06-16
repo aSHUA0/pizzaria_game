2ac9d4b4180951d44c634d7c1188cf1e661ece2a
aSHUA0 <guilhermemengo6@gmail.com>
2025-06-16 17:31:54 -0300

    create values file

---
+++ src/config/values.py
@@ -0,0 +1,12 @@
+# Configurações visuais
+altura_mesa = 300
+altura_cliente = 150
+largura_cliente = 80
+espaco_entre_clientes = 40
+
+#Pontuação do jogo
+score = 0
+score_win = 100
+score_loss = -50
+extra_time = 20
+bonus = 0
